e0e721eb77a6cd61b66908033235569e2bd26691
systemadmin-lab <rejuananik@gmail.com>
2025-05-06 04:12:30 +0600

    learning props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-function Square() {
+function Square({value}) {
   return (
     <>
       <button className="bg-white border border-gray-400 h-12 w-12 m-1">
-        X
+        {value}
       </button>
     </>
   );
@@ -12,31 +12,19 @@ function App() {
   return (
     <>
       <div>
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value='1' />
+        <Square value='2'/>
+        <Square value='3'/>
       </div>
       <div>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1">
-          X
-        </button>
+        <Square value='4' />
+        <Square value='5'/>
+        <Square value='6'/>
       </div>
       <div>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 h-12 w-12 m-1">
-          X
-        </button>
+        <Square value='7'/>
+        <Square value='8'/>
+        <Square value='9'/>
       </div>
     </>
   );
