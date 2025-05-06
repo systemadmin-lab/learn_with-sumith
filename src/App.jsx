2db49602e08c44d794084a064f853953a033f207
systemadmin-lab <rejuananik@gmail.com>
2025-05-06 11:21:01 +0600

    adding logics to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClicked() {
-   setValue('x')
-  }
+function Square({ value, onSquareClick }) {
   return (
     <>
       <button
-        onClick={handleClicked}
+        onClick={onSquareClick}
         className="bg-white border border-gray-400 h-12 w-12 m-1"
       >
         {value}
@@ -18,27 +14,66 @@ function Square() {
 }
 
 function App() {
-  const [squares,setSquares]=useState(Array(9).fill(null))
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  //for making decssion which will be priint x||0
+  const [xIsNext, setIsNext] = useState(true);
+
+  //we will send this button as  props to our child component
+  function handleClicked(index) {
+    const nextSquare = squares.slice();
+    if (squares[index]) {
+      return;
+    } else {
+      if (xIsNext) {
+        nextSquare[index] = "x";
+      } else {
+        nextSquare[index] = "o";
+      }
+    }
+    setSquares(nextSquare);
+    setIsNext(!xIsNext);
+  }
   return (
     <>
       <div className="flex">
-        <Square value={squares[0]}/>
-
-        <Square value={squares[1]} />
-        <Square value={squares[2]}/>
+        <Square value={squares[0]} onSquareClick={() => handleClicked(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClicked(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClicked(2)} />
       </div>
       <div className="flex">
-        <Square value={squares[3]} />
-        <Square value={squares[4]}/>
-        <Square value={squares[5]}/>
+        <Square value={squares[3]} onSquareClick={() => handleClicked(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClicked(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClicked(5)} />
       </div>
       <div className="flex">
-        <Square value={square[6]}/>
-        <Square value={squares[7]}/>
-        <Square value={squares[8]}/>
+        <Square value={squares[6]} onSquareClick={() => handleClicked(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClicked(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClicked(8)} />
       </div>
     </>
   );
 }
-
+// hhere we ar writing a function for checking winner 
+function calculateWinner(squares) {
+  //mark up cordinates 
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for(let i=0;i<lines.length;i++){
+    const[a,b,c] =lines[i];
+    //logics for checking winner
+    if(squares[a] && squares [a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }else{
+      return null
+    }
+  }
+}
 export default App;
